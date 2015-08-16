4cd5a45e941c8e87fbe323c5ff2f15fcf319f98c
John Nickerson <mokalusofborg@gmail.com>
2015-08-17 07:21:53 +1000

    Added basic spending commands. No aggregate yet.

---
+++ Maroon/Commands/Budget/RecordRepeatSpending.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AssimilationSoftware.Maroon.Commands.Budget
+{
+    public class RecordRepeatSpending : RecordSpending
+    {
+        public TimeSpan RepeatPeriod { get; set; }
+        public DateTime EndsAt { get; set; }
+    }
+}
---
+++ Maroon/Commands/Budget/RecordSpending.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AssimilationSoftware.Maroon.Commands.Budget
+{
+    public class RecordSpending : Command
+    {
+        public Guid RecordId { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public bool Income { get; set; }
+    }
+}
