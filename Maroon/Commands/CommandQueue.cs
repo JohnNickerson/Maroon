94e99322fe11b60b9c602ca5c2157cb25021c22c
John Nickerson <mokalusofborg@gmail.com>
2015-09-03 22:06:27 +1000

    Added files accidentally omitted from last commit.

---
+++ Maroon/Commands/CommandQueue.cs
@@ -0,0 +1,56 @@
+﻿using AssimilationSoftware.Maroon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AssimilationSoftware.Maroon.Commands
+{
+    public class CommandQueue
+    {
+        private string _path;
+        private SharpListSerialiser<Command> _serialiser;
+        private List<Command> _commands;
+
+        public CommandQueue(string path)
+        {
+            _path = path;
+            _serialiser = new SharpListSerialiser<Command>(path, false, (Command d) => d.CommandID.ToString());
+            Read();
+        }
+
+        public void Add(Command cmd)
+        {
+            _commands.Add(cmd);
+        }
+
+        public void Read()
+        {
+            _commands = _serialiser.Deserialise();
+        }
+
+        public void Write()
+        {
+            _serialiser.Serialise(_commands);
+        }
+
+        public IEnumerable<Command> Commands
+        {
+            get
+            {
+                return _commands.OrderBy(c => c.Timestamp);
+            }
+        }
+
+        public string Path
+        {
+            get { return _path; }
+            set
+            {
+                _path = value;
+                _serialiser.FileName = value;
+                Read();
+            }
+        }
+    }
+}
