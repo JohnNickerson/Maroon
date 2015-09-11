c0dd4da7e04b15137ea0493567ddd999e2d9d25b
John Nickerson <mokalusofborg@gmail.com>
2015-09-11 15:38:00 +1000

    Removed unused "Events" classes. Added an interface for command list storage mapping (yet to be implemented).

--- Maroon/Events/Notes/NoteCreated.cs
+++
@@ -1,9 +0,0 @@
-﻿using AssimilationSoftware.Maroon.Objects;
-
-namespace AssimilationSoftware.Maroon.Events.Notes
-{
-    public class NoteCreated : Event
-    {
-        public Note NewNote { get; set; }
-    }
-}
