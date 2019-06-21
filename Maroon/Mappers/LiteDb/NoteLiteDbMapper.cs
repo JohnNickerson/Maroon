c1a85c889968236c21f52379cd4d38f0d71f816a
John Nickerson <mokalusofborg@gmail.com>
2019-06-21 11:21:59 +1000

    Added a LiteDb mapper for the Note class, as a first test.

---
+++ Maroon/Mappers/LiteDb/NoteLiteDbMapper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AssimilationSoftware.Maroon.Interfaces;
+using AssimilationSoftware.Maroon.Model;
+using LiteDB;
+
+namespace AssimilationSoftware.Maroon.Mappers.LiteDb
+{
+    public class NoteLiteDbMapper : IMapper<Note>
+    {
+        private string _filename;
+        private readonly LiteDatabase _liteDb;
+        private readonly LiteCollection<Note> _allNotes;
+
+        public NoteLiteDbMapper(string filename)
+        {
+            _filename = filename;
+            _liteDb = new LiteDatabase(filename);
+            _allNotes = _liteDb.GetCollection<Note>("notes");
+            _allNotes.EnsureIndex(n => n.ID);
+        }
+
+        ~NoteLiteDbMapper()
+        {
+            _liteDb.Dispose();
+        }
+
+        public Note Load(Guid id)
+        {
+            return _allNotes.FindOne(n => n.ID == id);
+        }
+
+        public IEnumerable<Note> LoadAll()
+        {
+            return _allNotes.FindAll();
+        }
+
+        public void Save(Note item)
+        {
+            if (_allNotes.Exists(n => n.ID == item.ID))
+            {
+                _allNotes.Update(item);
+            }
+            else
+            {
+                _allNotes.Insert(item);
+            }
+        }
+
+        public void SaveAll(IEnumerable<Note> items)
+        {
+            foreach (var n in items)
+            {
+                // To insert or update as required.
+                Save(n);
+            }
+        }
+
+        public void Delete(Note item)
+        {
+            _allNotes.Delete(n => n.ID == item.ID);
+        }
+    }
+}
---
+++ UnitTests/NoteLiteDbTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using AssimilationSoftware.Maroon.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class NoteLiteDbTests
+    {
+        [TestMethod]
+        public void Round_Trip_Test()
+        {
+            var filename = "notes-test.db";
+            if (File.Exists(filename)) File.Delete(filename);
+            var mapper = new AssimilationSoftware.Maroon.Mappers.LiteDb.NoteLiteDbMapper(filename);
+
+            var note = new Note
+            {
+                ID = Guid.NewGuid(),
+                IsDeleted = false,
+                ImportHash = null,
+                LastModified = DateTime.Now,
+                ParentId = null,
+                PrevRevision = null,
+                RevisionGuid = Guid.NewGuid(),
+                Tags = new List<string>(new[] {"test", "tags"}),
+                Text = "This is a test item\nWith a newline",
+                Timestamp = DateTime.Now
+            };
+            mapper.Save(note);
+
+            var loaded = mapper.Load(note.ID);
+            Assert.AreEqual(note.ID, loaded.ID);
+        }
+    }
+}
